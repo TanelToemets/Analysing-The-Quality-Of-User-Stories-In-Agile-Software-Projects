a08bee88b7a2d3c3e6a2fdc6af2b0e1985688c56
tatoem <tatoem@ttu.ee>
2020-03-01 13:44:44 +0200

    Cleaning dnn - removing upper outliers
    Removing upper outliers by description length using standard deviation

--- data_cleaning/cleaning_dnn.py
+++ data_cleaning/cleaning_dnn.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import re
+import numpy as np
 
 
 #Getting user story data
@@ -15,6 +16,7 @@ story_done = df_project[(df_project['fields.issuetype.name'] == 'Story') & (df_p
 #Cleaning
 #Taking only description (user story) and indentification key
 project_save = story_done[['fields.description',  'key']]
+print(len(project_save))
 
 #Removing empty descriptions
 project_save = project_save[ project_save['fields.description'].notnull() ]
@@ -51,6 +53,18 @@ project_save = project_save.drop_duplicates()
 
 #Removing duplicate stories
 project_save = project_save.drop_duplicates(subset='fields.description', keep="first")
+print(len(project_save))
 
-#Writing cleaned data to a csv
-project_save.to_csv("C://Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/cleaned_input_data/jira-dnn-allus.csv", sep=',', encoding='utf-8', doublequote = True, header=False, index=False, line_terminator=",\n")
+
+#Removing upper outliers using standard deviation
+project_save['description_length'] = project_save['fields.description'].str.len()
+mean_story_length = np.mean(project_save['description_length'])
+standart_deviation_story_length = np.std(project_save['description_length'])
+project_save['suitable_title_length'] = project_save['description_length'].apply(lambda x: x if x < mean_story_length + 2 * standart_deviation_story_length else 0)
+project_save = project_save[project_save.suitable_title_length != 0]
+
+project_output = project_save[['fields.description',  'key']]
+print(len(project_output))
+
+#Writing 239 rows of cleaned data to a csv
+project_output.to_csv("C://Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/cleaned_input_data/jira-dnn-allus.csv", sep=',', encoding='utf-8', doublequote = True, header=False, index=False, line_terminator=",\n")
