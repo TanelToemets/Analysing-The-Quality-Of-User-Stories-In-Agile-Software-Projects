2f369adca7227b0ed4a95f24717012f454f19b85
tatoem <tatoem@ttu.ee>
2020-03-01 21:10:47 +0200

    Cleaning compass - removing upper outliers
    Removing upper outliers by description length using standard deviation

--- data_cleaning/cleaning_compass.py
+++ data_cleaning/cleaning_compass.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import re
+import numpy as np
 
 
 #Getting user story data
@@ -14,6 +15,7 @@ story_done = df_project[(df_project['issuetype.name'] == 'Story') & (df_project[
 #Cleaning
 #Taking only description (user story) and sprint key
 project_save = story_done[['description',  'key']]
+print(len(project_save))
 
 #Removing empty descriptions
 project_save = project_save[ project_save['description'].notnull() ]
@@ -52,6 +54,18 @@ project_save = project_save.drop_duplicates()
 
 #Removing duplicate stories
 project_save = project_save.drop_duplicates(subset='description', keep="first")
+print(len(project_save))
 
-#Writing cleaned data to a csv
-project_save.to_csv("C://Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/cleaned_input_data/jira-compass-allus.csv", sep=',', encoding='utf-8', doublequote = True, header=False, index=False, line_terminator=",\n")
+
+#Removing upper outliers using standard deviation
+project_save['description_length'] = project_save['description'].str.len()
+mean_story_length = np.mean(project_save['description_length'])
+standart_deviation_story_length = np.std(project_save['description_length'])
+project_save['suitable_title_length'] = project_save['description_length'].apply(lambda x: x if x < mean_story_length + 3 * standart_deviation_story_length else 0)
+project_save = project_save[project_save.suitable_title_length != 0]
+
+project_output = project_save[['description',  'key']]
+print(len(project_output))
+
+#Writing 93 rows of cleaned data to a csv
+project_output.to_csv("C://Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/cleaned_input_data/jira-compass-allus.csv", sep=',', encoding='utf-8', doublequote = True, header=False, index=False, line_terminator=",\n")
