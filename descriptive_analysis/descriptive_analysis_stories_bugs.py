20e00b2a4f96eb59d2543c0db6beaa56aa1a21f1
tatoem <tatoem@ttu.ee>
2020-03-07 23:31:33 +0200

    Descriptive analysis - nr of bugs

--- descriptive_analysis/descriptive_analysis_stories_bugs.py
+++ descriptive_analysis/descriptive_analysis_stories_bugs.py
@@ -5,22 +5,24 @@ import matplotlib.ticker as ticker
 #Possible projects
 #xd 
 #dnn
-#compass #failed
+#compass 
 #apstud
-#mesos #failed
+#mesos
 #mule
-#nexus #failed
+#nexus
 #timob
 #tistud
 
 projects = {
-    "xd": ("fields.issuetype.name", "fields.status.name", "Done", "jiradataset_issues.csv", "project"),
-	"dnn": ("fields.issuetype.name", "fields.status.statusCategory.name", "Done", "jiradataset_issues.csv", "project"),
-    "COMPASS": ("issuetype.name", "status.statusCategory.name", "Done", "compass_issues_extracted.csv", "project.key"),
-	"apstud": ("fields.issuetype.name", "fields.status.statusCategory.name", "Done", "jiradataset_issues.csv", "project"),
-    "mule": ("fields.issuetype.name", "fields.status.statusCategory.name", "Complete", "jiradataset_issues.csv", "project"),
-    "timob": ("fields.issuetype.name", "fields.status.statusCategory.name", "Done", "jiradataset_issues.csv", "project"),
-    "tistud": ("fields.issuetype.name", "fields.status.statusCategory.name", "Done", "jiradataset_issues.csv", "project"),
+    "xd":      ("fields.issuetype.name",  "fields.status.name",                 "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+	"dnn":     ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+    "COMPASS": ("issuetype.name",         "status.statusCategory.name",         "Done",      "compass_issues_extracted.csv", "project.key", "created"),
+	"apstud":  ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+    "mesos":  ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+    "mule":    ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Complete",  "jiradataset_issues.csv",       "project",     "fields.created"),
+    "nexus":   ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+    "timob":   ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",     "fields.created"),
+    "tistud":  ("fields.issuetype.name",  "fields.status.statusCategory.name",  "Done",      "jiradataset_issues.csv",       "project",      "fields.created"),
 }
 
 #print(projects["dnn"][1])
@@ -42,8 +44,11 @@ for x in project_list:
 
     #Number of stories after cleaning
     project_cleaned = pd.read_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/cleaned_input_data/jira-{0}-allus.csv".format(x), names=['title', 'key', 'z'])
-    print ("{0} {1} {2} {3}".format(x, "Nr of stories after cleaning:", len(project_cleaned), "\n"))
+    print ("{0} {1} {2}".format(x, "Nr of stories after cleaning:", len(project_cleaned)))
 
-    #TO BE COMPLETED: Number of bugs
-
+    #Number of bugs
+    project_bugs = df_project[(df_project[projects['{0}'.format(x)][0]] == 'Bug') & (df_project[projects['{0}'.format(x)][1]] == projects['{0}'.format(x)][2])] 
+    #Removing duplicate bugs by issutype, statusCategory, creation time and key     
+    project_bugs = project_bugs[[projects['{0}'.format(x)][0], projects['{0}'.format(x)][1], projects['{0}'.format(x)][5], 'key']].drop_duplicates()    
+    print ("{0} {1} {2} {3}".format(x, "Nr of bugs:", len(project_bugs), "\n"))
 
