142e545005c830f230f12a21c4642578e3f25a6d
tatoem <tatoem@ttu.ee>
2020-04-08 19:37:02 +0300

    Rework - formating plot, fixing special cases

--- rework_analysis/analysing_rework.py
+++ rework_analysis/analysing_rework.py
@@ -11,30 +11,30 @@ import datetime
 
 
 #Possible projects
-#xd           -
+#xd           +
 #dnn          +
-#COMPASS      -
+#COMPASS      +
 #apstud       +
 #mesos        +
 #mule         +
 #nexus        +
 #timob        +       
 #tistud       +
-project = 'xd'
+project = 'tistud'
 
 projects = {
-    "xd":      ("Done",  "In Progress" ), #65 Currently only Done -> In Progress ||Other: In Progress -> In PR | In Progress -> To Do | In PR -> In Progress | In PR -> To Do
-	"dnn":     ("XXXX", "Reopened"     ), #128 | Fine
-    "COMPASS": ("XXXX", "XXXX"         ), #NO DATA 
-	"apstud":  ("XXXX", "Reopened"     ), #49  |#Many are going from a Closed to Closed? | Otherwise fine
-    "mule":    ("Closed", "Reopened"   ), #4   | Fine
-    "nexus":   ("XXXX", "XXXX"         ), #0   |Reopened state and Closed->Open states exists but none of the stories we are analysing have this state 
-    "timob":   ("Closed", "Reopened"   ), #7   |#Two are going from a Closed to Closed?  | Otherwise fine
-    "tistud":  ("Closed", "Reopened"   ), #178 |#Many are going from a Closed to Closed? | Otherwise fine
+    "xd":      ("Done",  "In Progress", "jiradataset_changelog.csv" ), #65 Cnly Done -> In Progress| Fine  ||Other but not in our interest: In Progress -> In PR | In Progress -> To Do | In PR -> In Progress | In PR -> To Do
+	"dnn":     ("XXXX", "Reopened", "jiradataset_changelog.csv"     ), #128 | Fine
+    "COMPASS": ("XXXX", "XXXX", "compass_changelog_extracted.csv"   ), #3 
+	"apstud":  ("XXXX", "Reopened", "jiradataset_changelog.csv"     ), #49  |#Many are going from a Closed to Closed? | Otherwise fine
+    "mule":    ("Closed", "Reopened", "jiradataset_changelog.csv"   ), #4   | Fine
+    "nexus":   ("XXXX", "XXXX", "jiradataset_changelog.csv"         ), #0   |Reopened state and Closed->Open states exists but none of the stories we are analysing have this state 
+    "timob":   ("Closed", "Reopened", "jiradataset_changelog.csv"   ), #7   |#Two are going from a Closed to Closed?  | Otherwise fine
+    "tistud":  ("Closed", "Reopened", "jiradataset_changelog.csv"   ), #178 |#Many are going from a Closed to Closed? | Otherwise fine
 }
 
 #Read changelog file for rework data
-changelog_df = pd.read_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/datasets/jiradataset_changelog.csv")
+changelog_df = pd.read_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/datasets/"+projects['{0}'.format(project)][2])
 print(len(changelog_df))
 
 #Read the stories
@@ -56,7 +56,27 @@ rework_df = rework_df[rework_df['field'] == 'status']
 
 #rework_df = rework_df[rework_df['fromString'] == projects['{0}'.format(project)][0]]
 #Selecting Reopened stories
-rework_df = rework_df[rework_df['toString'] == projects['{0}'.format(project)][1]]
+
+#rework_df = rework_df[rework_df['toString'] == projects['{0}'.format(project)][1]]
+
+#finction for project specific state changes
+def manage_special_filters(project):
+    if project == 'COMPASS':
+        rework_df_managed = rework_df[rework_df['fromString'] == 'Closed']
+        rework_df_managed = rework_df_managed[(rework_df_managed['toString'] == 'Open') | (rework_df_managed['toString'] == 'In Progress') | (rework_df_managed['toString'] == 'Ready for Work') | (rework_df_managed['toString'] == 'Reopened')]
+        print('compass managed')
+        return rework_df_managed
+    elif project == 'xd':
+        rework_df_managed = rework_df[rework_df['fromString'] == 'Done']
+        rework_df_managed = rework_df_managed[rework_df_managed['toString'] == 'In Progress']
+        print('xd managed')
+        return rework_df_managed
+    else:
+        rework_df_managed = rework_df[rework_df['toString'] == projects['{0}'.format(project)][1]]
+        print('project managed')
+        return rework_df_managed
+
+rework_df = manage_special_filters(project)
 
 #Formating and indexing creationtime
 rework_df['created'] = pd.to_datetime(rework_df['created'], utc=True)
@@ -83,7 +103,12 @@ rework_df = rework_df.drop_duplicates(subset=['key'])
 print ("{0} {1} {2}".format(project, "Nr of rework cases:", len(rework_df)))
 
 #Plotting
+fig = pyplot.figure()
 rework_df.resample('SM')['rework_nr'].sum().plot()
+project_up = project.upper()
+fig.suptitle(project_up, fontsize=20)
+pyplot.xlabel('Time', fontsize=12)
+pyplot.ylabel('Percentage of all stories', fontsize=12)
 pyplot.show()
 
 #rework_df.to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/test.csv", sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
