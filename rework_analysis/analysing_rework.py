bf5bcd7d27198f0ec10a2406b6199477d09d3626
tatoem <tatoem@ttu.ee>
2020-04-18 23:22:10 +0300

    Analysing rework updated

--- rework_analysis/analysing_rework.py
+++ rework_analysis/analysing_rework.py
@@ -6,7 +6,7 @@ import datetime
 #created = date when change was made
 #fromString = start state
 #toString = end state
-#Closed -> Reopened   happens if client finds an error
+#Closed -> Reopened happens if client finds an error
 #Resolved -> Reopened happens if testing finds an errors
 
 
@@ -21,6 +21,7 @@ import datetime
 #timob        +       
 #tistud       +
 project = 'tistud'
+
 
 projects = {
     "xd":      ("Done",  "In Progress", "jiradataset_changelog.csv" ), #65 Cnly Done -> In Progress| Fine  ||Other but not in our interest: In Progress -> In PR | In Progress -> To Do | In PR -> In Progress | In PR -> To Do
@@ -48,8 +49,10 @@ stories_df = stories_df.drop_duplicates(subset=['key'])
 print(len(stories_df))
 
 #Merge story keys and rework data
-rework_df = pd.merge(stories_df, changelog_df, how='left', left_on='key', right_on='key')
-print(len(rework_df))
+# rework_df = pd.merge(stories_df, changelog_df, how='left', left_on='key', right_on='key')
+# print(len(rework_df))
+
+rework_df = changelog_df
 
 #Selecting the project
 rework_df = rework_df[rework_df['field'] == 'status']
@@ -66,13 +69,10 @@ def manage_special_filters(project):
         rework_df_managed = rework_df_managed[(rework_df_managed['toString'] == 'Open') | (rework_df_managed['toString'] == 'In Progress') | (rework_df_managed['toString'] == 'Ready for Work') | (rework_df_managed['toString'] == 'Reopened')]
         print('compass managed')
         return rework_df_managed
-    elif project == 'xd':
-        rework_df_managed = rework_df[rework_df['fromString'] == 'Done']
-        rework_df_managed = rework_df_managed[rework_df_managed['toString'] == 'In Progress']
-        print('xd managed')
-        return rework_df_managed
     else:
-        rework_df_managed = rework_df[rework_df['toString'] == projects['{0}'.format(project)][1]]
+        rework_df2 = rework_df[rework_df['project'] == project]
+        rework_df_managed = rework_df2[(rework_df2['fromString'] == 'Closed') | (rework_df2['fromString'] == 'Done') | (rework_df2['fromString'] == 'Resolved')]
+        rework_df_managed = rework_df_managed[(rework_df_managed['toString'] == 'Opened') | (rework_df_managed['toString'] == 'In Progress') | (rework_df_managed['toString'] == 'Reopened')]
         print('project managed')
         return rework_df_managed
 
@@ -81,9 +81,40 @@ rework_df = manage_special_filters(project)
 #Formating and indexing creationtime
 rework_df['created'] = pd.to_datetime(rework_df['created'], utc=True)
 rework_df = rework_df.set_index(pd.DatetimeIndex(rework_df['created']))
+
+def choose_active_development_periods(quality, project):
+    if project == 'dnn':
+        quality = quality['20130701':'20160101']
+        return quality
+    elif project == 'COMPASS':
+        quality = quality['20170920':'20180901']
+        return quality
+    elif project == 'apstud':
+        quality = quality['20110608':'20120620']
+        return quality
+    elif project == 'mule':
+        quality = quality['20130101':'20140901']
+        return quality
+    elif project == 'nexus':
+        #quality = quality['20140901':'20150401']
+        return quality
+    elif project == 'tistud':
+        quality = quality['20110101':'20140801']
+        return quality    
+    else:
+        return quality
+
+rework_df = choose_active_development_periods(rework_df, project)
+
+#Producing a input for furter WTLCC analysis
+WTLCC_input = rework_df
+WTLCC_input['rework_nr'] = 1
+WTLCC_input[['key','rework_nr','created']].to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/data/WTLCC_input_data/{0}-rework-for-WTLCC.csv".format(project), sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
+
 #Adding a row showing the percentage of each separate story 
-rework_df['rework_nr'] = 1/len(stories_df)
-rework_df = rework_df.drop_duplicates(subset=['key'])
+#rework_df['rework_nr'] = 1/len(stories_df)
+rework_df['rework_nr'] = 1
+#rework_df = rework_df.drop_duplicates(subset=['key'])
 
 
 #reading sprints data
@@ -107,8 +138,8 @@ fig = pyplot.figure()
 rework_df.resample('SM')['rework_nr'].sum().plot()
 project_up = project.upper()
 fig.suptitle(project_up, fontsize=20)
-pyplot.xlabel('Time', fontsize=12)
-pyplot.ylabel('Percentage of all stories', fontsize=12)
+pyplot.xlabel('Date', fontsize=12)
+pyplot.ylabel('Nr of rework cases', fontsize=12)
 pyplot.show()
 
 #rework_df.to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/test.csv", sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
