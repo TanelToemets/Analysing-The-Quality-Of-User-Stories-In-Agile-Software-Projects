fc7dd780be8aaca43c0c901d0b0895c824e97e25
tatoem <tatoem@ttu.ee>
2020-04-18 19:44:13 +0300

    WTLCC update

--- cross_correlation_analysis/WTLCC_bugs.py
+++ cross_correlation_analysis/WTLCC_bugs.py
@@ -6,8 +6,6 @@ import scipy.stats as stats
 sns.set_context('talk',font_scale=.8)
 import datetime
 from sklearn.metrics import r2_score
-# from sklearn.impute import KNNImputer
-# from sklearn.impute import SimpleImputer
 
 #Possible projects
 #xd 
@@ -38,56 +36,79 @@ def manage_compass_field_names(df, project):
     else:
         return df
 
+def choose_active_development_periods(quality, project):
+    if project == 'dnn':
+        quality = quality['20130701':'20160101']
+        return quality
+    elif project == 'compass':
+        quality = quality.reset_index(drop=True)
+        quality = quality[(quality['fields.created'] > '2017-09-20') & (quality['fields.created'] < '2018-09-01')]
+        quality = quality.set_index('fields.created')
+        return quality
+    elif project == 'apstud':
+        quality = quality.reset_index(drop=True)
+        quality = quality[(quality['fields.created'] > '2011-06-08') & (quality['fields.created'] < '2012-06-20')]
+        quality = quality.set_index('fields.created')
+        return quality
+    elif project == 'mule':
+        quality = quality.reset_index(drop=True)
+        quality = quality[(quality['fields.created'] > '2013-01-01') & (quality['fields.created'] < '2014-09-01')]
+        quality = quality.set_index('fields.created')
+        return quality
+    elif project == 'nexus':
+        quality = quality['20140901':'20150401']
+        return quality
+    elif project == 'tistud':
+        quality = quality['20110101':'20140801']
+        return quality    
+    else:
+        return quality
+
 #managing compass field names
 quality_scores_df = manage_compass_field_names(quality_scores_df, project)
 bugs_df = manage_compass_field_names(bugs_df, project)
-quality_scores_df.to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/test.csv", sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
+# print(quality_scores_df)
+# print(bugs_df)
 
-
-#indexing datetime
+#indexing creationtime
 quality_scores_df['fields.created'] = pd.to_datetime(quality_scores_df['fields.created'], utc=True)
 quality_scores_df = quality_scores_df.set_index(pd.DatetimeIndex(quality_scores_df['fields.created']))
 bugs_df['fields.created'] = pd.to_datetime(bugs_df['fields.created'], utc=True)
 bugs_df = bugs_df.set_index(pd.DatetimeIndex(bugs_df['fields.created']))
 
-#resamplying by SM (SM --> semi month (15th and end of month) | B-->business day | W-->week)
-resampled_qaulity = quality_scores_df.resample('B')['quality'].mean()#.ffill()#.plot()
+#taking only active development period
+quality_scores_df = choose_active_development_periods(quality_scores_df, project)
+bugs_df = choose_active_development_periods(bugs_df, project)
+print(quality_scores_df)
+print(bugs_df)
+
+#resamplying (SM-->semi month (15th and end of month) | B-->business day | W-->week))
+resampled_quality = quality_scores_df.resample('B')['quality'].mean().ffill()#.plot()
 resampled_bugs = bugs_df.resample('B')['bugnr'].sum()
-#plt.show()
+plt.show()
 
 #replacing empty string with NaN
 nan_value = float("NaN")
-resampled_qaulity.replace("", nan_value, inplace=True)
-#removing empty quality values
-# resampled_qaulity.dropna( inplace=True)
-# print(len(resampled_qaulity))
+resampled_quality.replace("", nan_value, inplace=True)
 
 #inner join on resampled quality and bugs
-quality_bugs_df = pd.merge(resampled_qaulity, resampled_bugs, left_index=True, right_index=True)
+quality_bugs_df = pd.merge(resampled_quality, resampled_bugs, left_index=True, right_index=True)
 print('len of quality_bugs_df')
 print(len(quality_bugs_df))
-
-
 
 ## IMPUTING MISSING VALUES. 
 ## reference: https://medium.com/@drnesr/filling-gaps-of-a-time-series-using-python-d4bfddd8c460
 ## Article scores different ways for imputation in order to suggest best imputation method for time-series analysis. Results suggest 'time' interpolation.  
 # print(quality_bugs_df.index)
-# Different ways for imputing
-# quality_bugs_df = quality_bugs_df.assign(RollingMean=quality_bugs_df['quality'].fillna(quality_bugs_df['quality'].rolling(24,min_periods=1,).mean()))
-# quality_bugs_df = quality_bugs_df.assign(InterpolateLinear=quality_bugs_df['quality'].interpolate(method='linear'))
-# quality_bugs_df = quality_bugs_df.assign(FillMean=quality_bugs_df['quality'].fillna(quality_bugs_df['quality'].mean()))
-# quality_bugs_df = quality_bugs_df.assign(FillMedian=quality_bugs_df['quality'].fillna(quality_bugs_df['quality'].median()))
 quality_bugs_df = quality_bugs_df.assign(InterpolateTime=quality_bugs_df['quality'].interpolate(method='time'))
 quality_bugs_df = quality_bugs_df.drop(['quality'], axis=1)
 quality_bugs_df = quality_bugs_df.rename(columns={'InterpolateTime': 'quality'})
-
 #If there was NaN values at the start of the time frame imputation was not possible, therefore NaN values will be dropped
 quality_bugs_df.dropna( inplace=True)
 print(len(quality_bugs_df))
 
 # Test files
-quality_bugs_df.to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/test.csv", sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
+# quality_bugs_df.to_csv("C:/Users/Tanel/Documents/Ylikool/Magister/Master Thesis/Analysing ASP Repo/test.csv", sep=',', encoding='utf-8', doublequote = True, header=True, index=False, line_terminator=",\n")
 
 
 #CROSS CORREALTION
@@ -113,8 +134,8 @@ def crosscorr(datax, datay, lag=0, wrap=False):
         return datax.corr(datay.shift(lag))
 
 
-# Windowed time lagged cross correlation
-# we have 673 business days in xd
+### Windowed time lagged cross correlation (WTLCC)
+
 
 #reference: https://stackoverflow.com/questions/31818050/round-number-to-nearest-integer
 def proper_round(num, dec=0):
@@ -126,9 +147,9 @@ def proper_round(num, dec=0):
 
 #Time window settings
 #no_splits = 4                                             #SETTING 1 - 4 time windows
-#no_splits = int(proper_round(len(quality_bugs_df)/66))    #SETTING 2 len/66B | Quarter | 1Q ~ 66B
+no_splits = int(proper_round(len(quality_bugs_df)/66))    #SETTING 2 len/66B | Quarter | 1Q ~ 66B
 #no_splits = int(proper_round(len(quality_bugs_df)/22))    #SETTING 3 len/22B | Months  | 1M ~ 22B 
-no_splits = int(proper_round(len(quality_bugs_df)/10))    #SETTING 4 len/10B | Sprint  |~2W = 10B 
+#no_splits = int(proper_round(len(quality_bugs_df)/10))    #SETTING 4 len/10B | Sprint  |~2W = 10B 
 
 
 #calculating cross correlation matrix
@@ -151,7 +172,7 @@ rss = pd.DataFrame(rss)
 #Plotting
 f,ax = plt.subplots(figsize=(10,5))
 sns.heatmap(rss,cmap='RdBu_r',ax=ax)
-ax.set(title=f'Windowed Time Lagged Cross Correlation',xlim=[0,u+u], xlabel='Offset',ylabel='Window epochs')
+ax.set(title=f'WTLCC for user story quality and number of bugs',xlim=[0,u+u], xlabel='Lag in business days',ylabel='Time window')
 ax.set_xticklabels([int(item-(u)) for item in ax.get_xticks()])
 # plt.ylabel('Sprints (2 week)', fontsize=12)
 project_up = project.upper()
